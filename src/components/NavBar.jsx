86fabe531e133bd7488f8fd2989d38595c09e518
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-11 18:52:13 -0500

    Modifications on Nav bar componet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,24 +3,24 @@ import logo from '../assets/images/react.svg'
 const NavBar = () => {
   return (
     <>  
-        <div className='flex justify-between bg-emerald-600 px-6 py-6 shadow-md'>
+        <div className='flex bg-emerald-600 px-6 py-6 shadow-md mb-10'>
             <img
-                className='h-10 w-auto mx-2 my-2'
+                className='h-10 w-auto mx-2 my-2 '
                 src = {logo}
             >
             </img> 
-            <nav className='flex justify-end w-full'>
+            <nav className='flex w-full justify-end'>
                 <a  href= '#'
-                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-emerald-900 hover:rounded-md'
+                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-white  hover:text-emerald-600 hover:rounded-md'
                 > Home
                 </a>
                 <a  href= '#'
-                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-emerald-900 hover:rounded-md'
+                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-white  hover:text-emerald-600 hover:rounded-md'
                 > Restaurants
                 </a>
                 <a  href= '#'
-                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-emerald-900 hover:rounded-md'
-                > Make Reservation
+                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-white  hover:text-emerald-600 hover:rounded-md'
+                > Reservation
                 </a>   
                 
             </nav>
