34d19a8c25830124269b4d847f8bdbf9a70d3ce3
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-12 22:21:53 -0500

    Component to display restaurants info

---
+++ src/components/RestaurantsListing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import restaurants from '../restaurants.json'
+
+const RestaurantsListing = () => {
+return (
+    <div className='flex justify-center'>
+        <h2 className='text-2xl font-semibold'>Best Recommendations</h2>
+    </div>
+)
+}
+export default RestaurantsListing
