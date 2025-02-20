9e83ba4c8e6ecbef018d08f1775e362881a56ee0
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-20 16:01:29 -0500

    Added active link on Navbar and conditional rendering for home and restaurant page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,36 @@
 import React from 'react'
 import logo from '../assets/images/react.svg'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom' //navlink provides isActive prop to work with that help on dynamically changing the style based on isActive 
 
 const NavBar = () => {
+    const linkClass = ({isActive}) => isActive ? 'text-emerald-600 bg-white rounded-md px-4 py-4 font-semibold scale-105 text-xl' : 'text-white text-xl font-medium px-4 py-4'
+
   return (
     <>  
         <div className='flex bg-emerald-600 px-6 py-6 shadow-md mb-10'>
-            <Link to = '/'>
-                <img
-                    className='h-10 w-auto mx-2 my-2 '
-                    src = {logo}
-                >
-                </img> 
-            </Link>
+            <div className='flex justify-center'>
+                <NavLink to = '/'>
+                    <img
+                        className='h-auto w-auto mx-2 my-2 border-2 border-emerald-300 bg-white rounded-full px-4 py-4'
+                        src = {logo}
+                    >
+                    </img> 
+                </NavLink>
+                <h3 className='px-4 py-4 text-2xl font-bold text-white'>ReserveIT</h3>
+            </div>
             <nav className='flex w-full justify-end'>
-                <Link  to= '/'
-                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-white  hover:text-emerald-600 hover:rounded-md'
+                <NavLink  to= '/'
+                    className= {linkClass}
                 > Home
-                </Link>
-                <Link  to= '/restaurants'
-                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-white  hover:text-emerald-600 hover:rounded-md'
+                </NavLink>
+                <NavLink  to= '/restaurants'
+                    className= {linkClass}
                 > Restaurants
-                </Link>
-                <Link  to= '/reservation'
-                    className='text-white text-xl font-medium px-4 py-4 transition durataion-300 hover:bg-white  hover:text-emerald-600 hover:rounded-md'
+                </NavLink>
+                <NavLink  to= '/reservation'
+                    className= {linkClass}
                 > Reservation
-                </Link>
+                </NavLink>
                 
             </nav>
         </div>
--- src/components/RestaurantsListing.jsx
+++ src/components/RestaurantsListing.jsx
@@ -2,25 +2,31 @@ import React from 'react'
 import RenderRestaurants from './RenderRestaurants'
 import restaurants from '../restaurants.json'
 import {Link} from 'react-router-dom'
-const RestaurantsListing = () => {
+const RestaurantsListing = ({isHome = false}) => {
+    const restautantListing = isHome ? restaurants.slice(0,6) : restaurants
+    const divClass = isHome ? 'rounded-md px-2 py-6 bg-emerald-600 mb-6 items-center flex flex-col' : 'rounded-md px-2 py-6 mb-6 items-center flex flex-col'
+    const h2Class = isHome ? 'text-2xl font-semibold text-center mb-6 text-white' : 'text-4xl font-bold text-center mb-6 text-emerald-500' 
+    const linkClass = isHome ? 'w-52 rounded-lg bg-white text-emerald-600 px-2 py-2 mb-4 mt-8 shadow-lg hover:scale-105 cursor-pointer text-center' : 'display-none'
 return (
     <>
-        <div className='rounded-md px-2 py-6 bg-emerald-600 mb-6 items-center flex flex-col'>
-            <h2 className='text-2xl font-semibold text-center mb-6 text-white'>Best Recommendations</h2>
+        <div className= {divClass}>
+            <h2 className= {h2Class}>{isHome ? 'Best Recommendations' : 'Find best restaurants around city'}</h2>
             <div className="grid grid-cols-3 gap-4 group">
-                <RenderRestaurants restaurants = {restaurants.slice(0,6)}> 
+                <RenderRestaurants restaurants = {restautantListing}> 
                     <Link className='w-1/2 rounded-lg justify-center text-center block mx-auto py-2 font-bold cursor-pointer bg-emerald-600 hover:shadow-lg text-white hover:scale-105'
-                        to = '/restaurants'
+                        to = '/restaurant-description'
                     >
                         Discover More  
                     </Link>
                 </RenderRestaurants>
-            </div>         
-            <Link className='w-52 rounded-lg bg-white text-emerald-600 px-2 py-2 mb-4 mt-8 shadow-lg hover:scale-105 cursor-pointer text-center'
-                to ='/restaurants'
-            >
-                    View All 
-            </Link>
+            </div> 
+            {isHome &&(
+                <Link className= {linkClass}
+                    to ='/restaurants'
+                >
+                        View All 
+                </Link>
+            )}        
         </div>
     </>
 )
