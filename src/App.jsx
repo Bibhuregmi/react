894e64d947cd23312ccacc5c84be0c2984687a8d
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-20 16:04:20 -0500

    Change to the entry point | added more pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import HomePage from './screens/HomePage';
 import MainLayout from './layout/MainLayout';
 import Restaurants from './screens/Restaurants'
 import Reservation from './screens/Reservation'
+import NotFound from './screens/NotFound';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path = '/' element = {<MainLayout /> }>
       <Route index element = {<HomePage />} />
       <Route path = '/restaurants' element = {<Restaurants />}/> 
       <Route path = '/reservation' element = {<Reservation />}/> 
+      <Route path='*' element= {<NotFound />}></Route>
     </Route>
   ) 
 );
