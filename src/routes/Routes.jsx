e1ae4915f008f236bd10f4ce2f2c1c5d73036793
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-20 21:28:07 -0500

    Routes for the site

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
+import HomePage from '../screens/HomePage'
+import MainLayout from '../layout/MainLayout';
+import Restaurants from '../screens/Restaurants'
+import Reservation from '../screens/Reservation'
+import RestaurantPage from '../screens/RestaurantPage'
+import NotFound from '../screens/NotFound';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = '/' element = {<MainLayout /> }>
+      <Route index element = {<HomePage />} />
+      <Route path = '/restaurants' element = {<Restaurants />}/> 
+      <Route path = '/reservation' element = {<Reservation />}/> 
+      <Route path='/restaurant-description' element = {<RestaurantPage />}/>
+      <Route path='*' element= {<NotFound />}></Route> 
+    </Route>
+  ) 
+);
+
+export default router
