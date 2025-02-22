34aa3e149dae32f93b47ce9047a7b9197de034b9
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-21 22:16:16 -0500

    Proxying

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,12 @@ export default defineConfig({
   ],
   server: {
     port: 7000,
+    proxy : {
+      '/api' : {
+        target : 'http://localhost:8000', 
+        changeOrigin: true, 
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
   }
 })
