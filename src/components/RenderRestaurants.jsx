7dce17401ac2e76470f3562dd02ca82d82f11c2c
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-22 21:32:42 -0500

    Changes and moved some logic

--- src/components/RenderRestaurants.jsx
+++ src/components/RenderRestaurants.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FaStar } from 'react-icons/fa'
 import { MdChair } from "react-icons/md"
 import { MdLocationOn } from "react-icons/md"
 import { Link } from 'react-router-dom'
 
-const RenderRestaurants = ({ restaurants, children }) => {  
-    return restaurants.map((restaurant) => (
-        <div key = {restaurant.id} className='bg-white p-4 rounded-lg shadow-lg '>
-            <h2 className='text-2xl font-bold mb-2'>{restaurant.name}</h2>
-            <p className=' mb-2'>{restaurant.description}</p>
+const RenderRestaurants = ({ restaurants, children }) => { 
+    
+    const[showFullDescription, setShowFullDescription] = useState(false); 
+
+    let details = restaurants.description; 
+    console.log(details)
+
+    if(!showFullDescription){
+        details = details.substring(0,90) + '...'
+    }
+
+    return (
+        <div key = {restaurants.id} className='bg-white p-4 rounded-lg shadow-lg '>
+            <h2 className='text-2xl font-bold mb-2'>{restaurants.name}</h2>
+            <p className=' mb-2'>{details}</p>
+            <button
+                onClick={() => setShowFullDescription((prevState) => !prevState)}
+                className='mb-3 text-gray-400 hover:text-gray-700 text-sm'
+            >
+                {showFullDescription ? 'Less' : 'More'}
+            </button>
             <div className='flex justify-baseline'>
-            <MdLocationOn className='inline text-xl mt-0.5 mr-0.5'/><p className='text-gray-600 mb-2'>{restaurant.location}</p>
+            <MdLocationOn className='inline text-xl mt-0.5 mr-0.5'/><p className='text-gray-600 mb-2'>{restaurants.location}</p>
             </div>
             <div className = 'flex justify-between mb-2'>
                 
-                <p className='py-2'><span className='text-yellow-700'><FaStar/>{restaurant.rating}</span></p>   
-                <p className='py-2'><MdChair/>{restaurant.capacity}</p>   
+                <p className='py-2'><span className='text-yellow-700'><FaStar/>{restaurants.rating}</span></p>   
+                <p className='py-2'><MdChair/>{restaurants.capacity}</p>   
             </div>
             <Link className='w-1/2 rounded-lg justify-center text-center block mx-auto py-2 font-bold cursor-pointer bg-emerald-600 hover:shadow-lg text-white hover:scale-105'
-                to = {`/restaurants/${restaurant.id}`}
+                to = {`/restaurants/${restaurants.id}`}
             >
                 Discover More  
             </Link>
             {children}  
         </div>
-    ))
+    )
 }
 export default RenderRestaurants
--- src/components/RestaurantsListing.jsx
+++ src/components/RestaurantsListing.jsx
@@ -8,15 +8,15 @@ const RestaurantsListing = ({isHome = false}) => {
     const [loading, setLoading] = useState(true); 
 
     //logics for  conditional rendering 
-    const restaurantListing = isHome ? restaurants.slice(0,6) : restaurants
     const divClass = isHome ? 'rounded-md px-2 py-6 bg-emerald-600 mb-6 items-center flex flex-col' : 'rounded-md px-2 py-6 mb-6 items-center flex flex-col'
     const h2Class = isHome ? 'text-2xl font-semibold text-center mb-6 text-white' : 'text-4xl font-bold text-center mb-6 text-emerald-500' 
     const linkClass = isHome ? 'w-52 rounded-lg bg-white text-emerald-600 px-2 py-2 mb-4 mt-8 shadow-lg hover:scale-105 cursor-pointer text-center' : 'display-none'
     //useEffect in react is used to return a cleanup function for updates or unmounting components, and react expects it to be synchronous and making it async, it must return a promise which is against the rule of react 
     useEffect(() => {
         const fetchRestaurants = async () => {
+            const apiUrl = isHome ? '/api/restaurants?_limit=6' : '/api/restaurants'
             try {
-                const res = await fetch('/api/restaurants')
+                const res = await fetch(apiUrl)
                 const data = await res.json();
                 console.log(data)
                 setRestaurants(data); 
@@ -35,8 +35,9 @@ return (
                 {
                     loading ? (<Spinner/>) : (
                         <div className="grid grid-cols-3 gap-4 group">
-                        <RenderRestaurants restaurants = {restaurantListing}> 
-                        </RenderRestaurants>
+                            {restaurants.map((restaurant) => (
+                                <RenderRestaurants key={restaurant.id} restaurants = {restaurant}/> 
+                            ))}
                         </div>
                     )
                 } 
